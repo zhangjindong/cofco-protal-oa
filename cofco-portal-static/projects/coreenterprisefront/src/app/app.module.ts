ee402e520047cd5fa7ac1aefdc72af486a0bafcd
zhangjindong <zhangjindong@gcstgroup.com>
2020-10-23 11:27:10 +0800

    npx ng  add ng-zorro-antd --project=coreenterprisefront

--- cofco-portal-static/projects/coreenterprisefront/src/app/app.module.ts
+++ cofco-portal-static/projects/coreenterprisefront/src/app/app.module.ts
@@ -8,6 +8,13 @@ import { EnvService } from './shared/env.service';
 import { initializer } from './utils/app-init';
 import { SharedModule } from './shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NZ_I18N, zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,6 +24,8 @@ import { HttpClientModule } from '@angular/common/http';
     SharedModule,
     HttpClientModule,
     KeycloakAngularModule,
+    NzLayoutModule,
+    NzMenuModule,
   ],
   providers: [
     {
